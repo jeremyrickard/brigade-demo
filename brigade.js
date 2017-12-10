3d4a8227dd51b11fc3ad3949246d68aa9050805d
Jeremy Rickard <jeremy.r.rickard@gmail.com>
2017-12-10 14:07:02 -0800

    Updated brigade.js

--- brigade.js
+++ brigade.js
@@ -14,40 +14,21 @@ events.on("pull_request", (event,project) => {
                 GH_STATE: "success",
                 GH_DESCRIPTION: "brigade says YES!",
                 GH_CONTEXT: "brigade",
-
-                // We get the token from the project's secrets section.
                 GH_TOKEN: project.secrets.githubToken, // YOU MUST SET THIS IN YOUR PROJECT
-
-                // We set this because 'exec' doesn't have a real commit. Normally you
-                // would use e.commit.
                 GH_COMMIT: event.commit
 
         }
 
-        var group = new Group()
-        group.add(testJob)
-        group.add(reporter)
-        console.log("before")
-
-
         testJob.run().then(results => {
-                console.log("good")
 		reporter.run()
         }).catch(error => {
-		console.log(error)
 		reporter.env = {
                		GH_REPO: project.repo.name,
                 	GH_STATE: "failure",
                 	GH_DESCRIPTION: "build failed",
                 	GH_CONTEXT: "brigade",
-
-                	// We get the token from the project's secrets section.
                 	GH_TOKEN: project.secrets.githubToken, // YOU MUST SET THIS IN YOUR PROJECT
-
-                	// We set this because 'exec' doesn't have a real commit. Normally you
-                	// would use e.commit.
                 	GH_COMMIT: event.commit
-
         	}
 		reporter.run().then( () => {
 			throw error
